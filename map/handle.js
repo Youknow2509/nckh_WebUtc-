c0da7d77af8724a71222789fa5e9b4015c2d5575
V <lytranvinh.work@gmail.com>
2024-03-15 00:27:18 +0700

    [Upgarde]: assest map

--- map/handle.js
+++ map/handle.js
@@ -3,15 +3,14 @@ var toa = [];
 
 var toaA8 = {
     name: "A8",
-    img: "https://storage.googleapis.com/digital-platform/hinh_anh_top_10_toa_nha_cao_nhat_viet_nam_hien_nay_landmark_81_dung_thu_may_so_1_f5980c8db0/hinh_anh_top_10_toa_nha_cao_nhat_viet_nam_hien_nay_landmark_81_dung_thu_may_so_1_f5980c8db0.jpg",
+    img: "/Assets/Map/A8/A8.png",
     dstang: [
-        {name: "Tang 1", phong: [{name: "101", img:""}, {name: "102", img:""}, {name: "103", img:""}, {name: "104", img:""}, {name: "105", img:""}]},
-        {name: "Tang 2", phong: [{name: "201", img:""}, {name: "202", img:""}, {name: "203", img:""}, {name: "204", img:""}, {name: "205", img:""}]},
-        {name: "Tang 3", phong: [{name: "301", img:""}, {name: "302", img:""}, {name: "303", img:""}, {name: "304", img:""}, {name: "305", img:""}]},
-        {name: "Tang 4", phong: [{name: "401", img:""}, {name: "402", img:""}, {name: "403", img:""}, {name: "404", img:""}, {name: "405", img:""}]},
-        {name: "Tang 5", phong: [{name: "501", img:""}, {name: "502", img:""}, {name: "503", img:""}, {name: "504", img:""}, {name: "505", img:""}]},
-        {name: "Tang 6", phong: [{name: "601", img:""}, {name: "602", img:""}, {name: "603", img:""}, {name: "604", img:""}, {name: "605", img:""}]},
-        {name: "Tang 7", phong: [{name: "701", img:""}, {name: "702", img:""}, {name: "703", img:""}, {name: "704", img:""}, {name: "705", img:""}]},
+        {name: "Tang 2", phong: [{name: "201", img:"/Assets/Map/A8/2/201.png"}, {name: "202", img:"/Assets/Map/A8/2/202.png"}, {name: "203", img:"/Assets/Map/A8/2/203.png"}, {name: "204", img:"/Assets/Map/A8/2/204.png"}, {name: "205", img:"/Assets/Map/A8/2/205.png"}, {name: "206", img:"/Assets/Map/A8/2/206.png"}, {name: "20N", img:"/Assets/Map/A8/2/20N.png"}]},
+        {name: "Tang 3", phong: [{name: "301", img:"/Assets/Map/A8/3/301.png"}, {name: "302", img:"/Assets/Map/A8/3/302.png"}, {name: "303", img:"/Assets/Map/A8/3/303.png"}, {name: "304", img:"/Assets/Map/A8/3/304.png"}, {name: "305", img:"/Assets/Map/A8/3/305.png"}, {name: "306", img:"/Assets/Map/A8/3/306.png"}, {name: "30N", img:"/Assets/Map/A8/3/30N.png"}]},
+        {name: "Tang 4", phong: [{name: "401", img:"/Assets/Map/A8/4/401.png"}, {name: "402", img:"/Assets/Map/A8/4/402.png"}, {name: "403", img:"/Assets/Map/A8/4/403.png"}, {name: "404", img:"/Assets/Map/A8/4/404.png"}, {name: "405", img:"/Assets/Map/A8/4/405.png"}, {name: "406", img:"/Assets/Map/A8/4/406.png"}, {name: "40N", img:"/Assets/Map/A8/4/40N.png"}]},
+        {name: "Tang 5", phong: [{name: "501", img:"/Assets/Map/A8/5/501.png"}, {name: "502", img:"/Assets/Map/A8/5/502.png"}, {name: "503", img:"/Assets/Map/A8/5/503.png"}, {name: "504", img:"/Assets/Map/A8/5/504.png"}, {name: "505", img:"/Assets/Map/A8/5/505.png"}, {name: "506", img:"/Assets/Map/A8/5/506.png"}, {name: "50N", img:"/Assets/Map/A8/5/50N.png"}]},
+        {name: "Tang 6", phong: [{name: "6DT", img:"/Assets/Map/A8/6/6DT.png"}, {name: "6TV", img:"/Assets/Map/A8/6/6TV.png"}]},
+        {name: "Tang 7", phong: [{name: "7TV", img:"/Assets/Map/A8/7/7TV.png"}]}
     ]
 };
 // Thêm các toà vào mảng toa
@@ -32,10 +31,8 @@ document.getElementById('find').addEventListener('click', function() {
     if (res.c) {
         document.getElementById('addToa').innerHTML = "Toà: " + res.toa.name;
         document.getElementById('addPhong').innerHTML = "Phòng: " + res.phong.name;
-        document.getElementById('imgToa').src = "https://storage.googleapis.com/digital-platform/hinh_anh_top_10_toa_nha_cao_nhat_viet_nam_hien_nay_landmark_81_dung_thu_may_so_1_f5980c8db0/hinh_anh_top_10_toa_nha_cao_nhat_viet_nam_hien_nay_landmark_81_dung_thu_may_so_1_f5980c8db0.jpg"  
-        // Để tạm link kia để gán sau + res.toa.img;
-        document.getElementById('imgPhong').src = "https://storage.googleapis.com/digital-platform/hinh_anh_phong_ngu_master_la_gi_nhung_luu_y_khi_thiet_ke_phong_ngu_master_so_4_cef2fb3218/hinh_anh_phong_ngu_master_la_gi_nhung_luu_y_khi_thiet_ke_phong_ngu_master_so_4_cef2fb3218.jpg";
-        // Để tạm link kia để gán sau + res.phong.img;
+        document.getElementById('imgToa').src = res.toa.img
+        document.getElementById('imgPhong').src = res.phong.img
         handleOpenModal();
     } else {
         alert("Vui lòng kiểm tra lại thông tin!");
